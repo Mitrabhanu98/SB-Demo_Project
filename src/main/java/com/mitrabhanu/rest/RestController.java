b9b1de4d02fd29336c39aa10ce4480b59553b95a
Mitrabhanu <1998nitrabhanu@gmail.com>
2024-08-05 07:07:04 +0530

    first commit

---
+++ src/main/java/com/mitrabhanu/rest/RestController.java
@@ -0,0 +1,6 @@
+package com.mitrabhanu.rest;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+}
---
+++ src/test/java/com/mitrabhanu/SbDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitrabhanu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
